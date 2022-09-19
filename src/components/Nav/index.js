318c24e5dd29fd4f6319f614aacda33bca65fddc
rudy <rudy@macbook-air-de-margaux.home>
2022-09-19 12:20:01 +0200

    create navbar

---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import "./styles.scss";
+
+function Nav() {
+    return (
+  <div className="navBar">
+    <ul className="navBar-lists">
+        <li className="navBar-list">Home</li>
+        <li className="navBar-list">About</li>
+        <li className="navBar-list">Skills</li>
+        <li className="navBar-list">Contact</li>
+    </ul>
+  </div>
+  );
+}
+
+export default Nav;
